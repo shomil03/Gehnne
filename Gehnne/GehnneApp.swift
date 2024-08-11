7378b6354eb1321430befd2ec5bef7a6d29fa442
shomil03 <shomil02020@gmail.com>
2024-08-12 01:48:26 +0530

    Initial Commit

---
+++ Gehnne/GehnneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GehnneApp.swift
+//  Gehnne
+//
+//  Created by Shomil Singh on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GehnneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
