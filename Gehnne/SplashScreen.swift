cfb5734c17ecddc0ad7a90c0c090a4868a75b2f7
shomil03 <shomil02020@gmail.com>
2024-08-17 15:04:55 +0530

    added navigation after splashscreen

--- Gehnne/SplashScreen.swift
+++ Gehnne/SplashScreen.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SplashScreen: View {
     @State private var animateStrip = false
+    @State private var navigate = false
     let lightBeige = Color(red: 240/255, green: 235/255, blue: 221/255)
     let mutedGold = Color(red: 193/255, green: 179/255, blue: 136/255)
     var body: some View {
@@ -16,29 +17,39 @@ struct SplashScreen: View {
         //            .font(.largeTitle)
         //            .fontWeight(.heavy)
         //            .fontWidth(.expanded)
-        Text("Gehnne")
-            .font(.system(size: 60, weight: .bold, design: .default))
-            .foregroundColor(mutedGold)
-            .overlay(
-                GeometryReader { geometry in
-                    // Slanting line inside the text bounds
-                    Rectangle()
-                        .fill(lightBeige)
-                        .frame(width: geometry.size.width * 2.5, height: 5)
-                        .rotationEffect(.degrees(-30))
-                        .offset(x: animateStrip ? geometry.size.width : -geometry.size.width * 1.5)
-                        
-                        .onAppear {
-                            withAnimation(.easeInOut(duration: 3).repeatForever(autoreverses: false)){
-                                animateStrip = true
-                            }
+        if(!navigate){
+            VStack {
+                Text("Gehnne")
+                    .font(.system(size: 60, weight: .bold, design: .default))
+                    .foregroundColor(mutedGold)
+                    .overlay(
+                        GeometryReader { geometry in
+                            // Slanting line inside the text bounds
+                            Rectangle()
+                                .fill(lightBeige)
+                                .frame(width: geometry.size.width * 2.5, height: 5)
+                                .rotationEffect(.degrees(-30))
+                                .offset(x: animateStrip ? geometry.size.width : -geometry.size.width * 1.5)
+                            
+                                .onAppear {
+                                    withAnimation(.easeInOut(duration: 3).repeatForever(autoreverses: false)){
+                                        animateStrip = true
+                                        DispatchQueue.main.asyncAfter(deadline: .now()+3){
+                                            navigate = true
+                                        }
+                                    }
+                                }
                         }
-                }
-                    .mask(
-                        Text("Gehnne")
-                            .font(.system(size: 60, weight: .bold, design: .default))
-                    ) // Mask the strip to the text bounds
-            )
+                            .mask(
+                                Text("Gehnne")
+                                    .font(.system(size: 60, weight: .bold, design: .default))
+                            ) // Mask the strip to the text bounds
+                    )
+            }
+        }
+        else{
+            ContentView()
+        }
     
             
     }
